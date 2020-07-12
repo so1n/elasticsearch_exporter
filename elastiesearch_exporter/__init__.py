c422d01f2b1f717a961d3c45bf18e8b832d78bd8
So1n <qaz6803609@163.com>
2020-07-12 16:19:55 +0800

    Modify, feat, add init

---
+++ elastiesearch_exporter/__init__.py
@@ -0,0 +1,62 @@
+import argparse
+import logging
+import os
+
+import yaml
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from elasticsearch import Elasticsearch
+from prometheus_client.exposition import start_http_server
+from prometheus_client.core import REGISTRY
+
+from elastiesearch_exporter.collector import Es, QueryMetricCollector
+from elastiesearch_exporter.utils import shutdown
+
+
+@shutdown()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", default='')
+    parser.add_argument("--es_cluster")
+    parser.add_argument("--listen_port", default=9206)
+    parser.add_argument("--log_level", default='INFO')
+    parser.add_argument("--apscheduler_log_level", default='WARNING')
+    args, unknown = parser.parse_known_args()
+
+    es_cluster_list: list = args.es_cluster.split(',')
+    listen_port: int = int(args.listen_port)
+    config_filename_path: str = args.config
+    log_level: str = args.log_level
+    apscheduler_log_level: str = args.apscheduler_log_level
+
+    logging.basicConfig(
+        format='[%(asctime)s %(levelname)s %(process)d] %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S',
+        level=getattr(logging, log_level.upper()))
+
+    logging.info('Starting server...')
+    start_http_server(listen_port)
+    logging.info(f'Server started on port {listen_port}')
+
+    es_client = Elasticsearch(es_cluster_list, verify_certs=False)
+
+    # REGISTRY.register(ClusterHealthCollector(es_client, 10, 'INFO'))
+    # REGISTRY.register(IndicesStatsCollector(es_client, 10))
+    # REGISTRY.register(NodesStatsCollector(es_client, 10))
+
+    if not config_filename_path:
+        config_filename_path = './config.yaml'
+    if os.path.exists(config_filename_path):
+        with open(config_filename_path, 'r') as config_file:
+            custom_metric_config = yaml.load(
+                config_file, Loader=yaml.FullLoader
+            )
+        es = Es(es_client, 10)
+        REGISTRY.register(QueryMetricCollector(es))
+
+        scheduler = BackgroundScheduler()
+        for job, interval, name in es.gen_job(custom_metric_config):
+            scheduler.add_job(job, 'interval', seconds=interval, name=name)
+        # logging.getLogger('apscheduler.executors.default').setLevel(
+        #     getattr(logging, apscheduler_log_level))
+        scheduler.start()
