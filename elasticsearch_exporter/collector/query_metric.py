7cdd2a057f2d3167eee36f6f0b6fb17b25bd333f
So1n <qaz6803609@163.com>
2020-10-14 22:01:52 +0800

    Modify, query_metric suuport type hints

--- elasticsearch_exporter/collector/query_metric.py
+++ elasticsearch_exporter/collector/query_metric.py
@@ -1,20 +1,22 @@
 from collections import OrderedDict
 from functools import partial
+from typing import Any, Callable, Dict, Generator, List, Optional, Tuple
 
+from elasticsearch import Elasticsearch
 from prometheus_client.core import GaugeMetricFamily
 
 from .base import interval_handle
 
 
 class QueryMetricCollector(object):
-    def __init__(self, es_client):
-        self.es_client = es_client
-        self.custom_metric_dict = {}
+    def __init__(self, es_client: 'Elasticsearch'):
+        self.es_client: 'Elasticsearch' = es_client
+        self.custom_metric_dict: Dict[str, GaugeMetricFamily] = {}
 
-    def gen_job(self, config):
-        global_c = config['global']
+    def gen_job(self, config: Dict[str, Any]) -> Generator[Tuple[partial, Dict[str, Any]], None, None]:
+        global_c: Dict[str, Any] = config['global']
         for metric_config_dict in config['metrics']:
-            _interval = metric_config_dict.get('interval', global_c['interval'])
+            _interval: str = metric_config_dict.get('interval', global_c['interval'])
             if 'timeout' not in metric_config_dict:
                 metric_config_dict['timeout'] = global_c['timeout']
             if 'jitter' not in metric_config_dict:
@@ -26,13 +28,15 @@ class QueryMetricCollector(object):
                 metric_config_dict
             )
 
-    def _aggregations_handle(self, aggregations_dict, label_dict=None):
+    def _aggregations_handle(
+            self, aggregations_dict: Dict[str, Dict[str, Any]], label_dict: Optional[OrderedDict[str, str]] = None
+    ) -> Generator[Tuple[OrderedDict[str, str], int], None, None]:
         for metric_key, metric_dict in aggregations_dict.items():
             if metric_key == 'key' or metric_key == 'doc_count':
                 continue
             if label_dict is None:
-                label_dict = OrderedDict()
-            bucket_dict_list = metric_dict["buckets"]
+                label_dict: OrderedDict[str, str] = OrderedDict()
+            bucket_dict_list: List[Dict[str, Any]] = metric_dict["buckets"]
             for bucket_dict in bucket_dict_list:
                 label_dict[metric_key] = bucket_dict['key']
                 if len(bucket_dict) > 2:
@@ -41,22 +45,22 @@ class QueryMetricCollector(object):
                         label_dict=label_dict
                     )
                 else:
-                    metric_value = bucket_dict['doc_count']
+                    metric_value: int = bucket_dict['doc_count']
                     yield label_dict, metric_value
 
-    def get_metric(self, metric_config_dict):
-        response = self.es_client.search(
+    def get_metric(self, metric_config_dict: Dict[str, Any]):
+        response: Dict[str, Any] = self.es_client.search(
             index=metric_config_dict['index'],
             body=metric_config_dict['query_json'],
             request_timeout=metric_config_dict['timeout']
         )
-        metric = metric_config_dict["metric"].format(**metric_config_dict)
+        metric: str = metric_config_dict["metric"].format(**metric_config_dict)
         metric = metric.replace("*", "")
         if response['timed_out']:
             return
 
-        key = metric + '_total_milliseconds'
-        g = GaugeMetricFamily(
+        key: str = metric + '_total_milliseconds'
+        g: 'GaugeMetricFamily' = GaugeMetricFamily(
             key,
             metric_config_dict['doc'] + ' total_milliseconds',
             value=response['took']
@@ -67,8 +71,8 @@ class QueryMetricCollector(object):
         if isinstance(total, dict):
             total = total['value']
 
-        key = metric + '_hits_total'
-        g = GaugeMetricFamily(
+        key: str = metric + '_hits_total'
+        g: 'GaugeMetricFamily' = GaugeMetricFamily(
             key,
             metric_config_dict['doc'] + ' hits_total',
             total
@@ -76,8 +80,8 @@ class QueryMetricCollector(object):
         self.custom_metric_dict[key] = g
 
         if 'aggregations' in response:
-            key = metric + '_aggregations'
-            g = None
+            key: str = metric + '_aggregations'
+            g: 'Optional[GaugeMetricFamily]' = None
             for label_dict, metric_value in self._aggregations_handle(response['aggregations']):
                 if not g:
                     label_key_list = label_dict.keys()
@@ -89,7 +93,7 @@ class QueryMetricCollector(object):
                 g.add_metric(label_dict.values(), metric_value)
             self.custom_metric_dict[key] = g
 
-    def collect(self):
+    def collect(self) -> Generator[GaugeMetricFamily, None, None]:
         query_metrics = self.custom_metric_dict.copy()
-        for metric_dict in query_metrics.values():
-            yield metric_dict
+        for metric in query_metrics.values():
+            yield metric
